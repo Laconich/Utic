00906a5d4eb4effb6586229c56e8ee9c5508976b
Laconich <jlaconich97@gmail.com>
2024-01-18 18:21:31 -0300

    Se arega validaciones

--- pitagora.js
+++ pitagora.js
@@ -1,30 +1,24 @@
-// function calcularHipotenusa(catetoA, catetoB) {
-//     // Aplicar el teorema de Pitágoras: a^2 + b^2 = c^2
-//     const cuadradoCatetoA = Math.pow(catetoA, 2)
-//     const cuadradoCatetoB = Math.pow(catetoB, 2)
-  
-//     // Calcular la hipotenusa (c)
-//     const hipotenusa = Math.sqrt(cuadradoCatetoA + cuadradoCatetoB)
-  
-//     return hipotenusa
-//   }
-  
-//   // Ejemplo de uso
-//   const catetoA = 3
-//   const catetoB = 4
-  
-//   const hipotenusa = calcularHipotenusa(catetoA, catetoB)
-//   console.log(`La hipotenusa es: ${hipotenusa}`)
-
 // Solicitar al usuario que ingrese el primer cateto
-var cateto1 = parseFloat(prompt("Ingresa el valor del primer cateto:"));
+var inputCateto1 = prompt("Ingresa el valor del primer cateto:");
+var cateto1 = parseFloat(inputCateto1);
 
-// Solicitar al usuario que ingrese el segundo cateto
-var cateto2 = parseFloat(prompt("Ingresa el valor del segundo cateto:"));
+// Validar el primer cateto
+if (isNaN(cateto1)) {
+  alert("Por favor, ingresa un valor numérico para el primer cateto.");
+} else {
+  // Solicitar al usuario que ingrese el segundo cateto
+  var inputCateto2 = prompt("Ingresa el valor del segundo cateto:");
+  var cateto2 = parseFloat(inputCateto2);
 
-// Calcular la hipotenusa usando la fórmula de Pitágoras
-var hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+  // Validar el segundo cateto
+  if (isNaN(cateto2)) {
+    alert("Por favor, ingresa un valor numérico para el segundo cateto.");
+  } else {
+    // Calcular la hipotenusa usando la fórmula de Pitágoras
+    var hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
 
-// Mostrar el resultado al usuario
-alert("La hipotenusa es: " + hipotenusa);
-console.log(`La hipotenusa es: ${hipotenusa}`)
+    // Mostrar el resultado al usuarioS
+    alert("La hipotenusa es: " + hipotenusa);
+    console.log(`La hipotenusa es: ${hipotenusa}`);
+  }
+}
