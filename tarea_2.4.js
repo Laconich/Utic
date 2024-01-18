5d669ac246f6220e8b6bc962523835708312fc89
Laconich <jlaconich97@gmail.com>
2024-01-18 19:11:18 -0300

    tarea 2.4

---
+++ tarea_2.4.js
@@ -0,0 +1,18 @@
+// Crear variables para tres números: a, b, c
+var a = 10;
+var b = 5;
+var c = 3;
+
+// Sumar b en a
+a += b;
+
+// Dividir a con c
+a /= c;
+
+// Reemplazar el valor de c con el módulo de c y b
+c = c % b;
+
+// Imprimir los tres números en consola
+console.log("a: " + a);
+console.log("b: " + b);
+console.log("c: " + c);
