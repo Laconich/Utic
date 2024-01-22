959bd4f12135ae872859b450d4b5fb653aa4cf81
Laconich <jlaconich97@gmail.com>
2024-01-22 19:12:37 -0300

    Arrays

---
+++ Array.js
@@ -0,0 +1,8 @@
+let razadeperros = ['perro','perrito','perrote',1200,false,null];
+console.log({razadeperros});
+
+//const ultimoElemento = razadeperros
+
+//Uso de la propiedad lenght
+
+console.log(razadeperros.length);
---
+++ Ejercicio3.1.js
@@ -0,0 +1,11 @@
+// Crear un array para la lista de compras
+var listaCompras = ["Manzanas", "Leche", "Pan"];
+
+// Revisar la longitud de la lista
+console.log("Longitud de la lista de compras: " + listaCompras.length);
+console.log("Lista de compras:", listaCompras);
+// Actualizar uno de los elementos
+listaCompras[1] = "Yogur";  // Actualizar el segundo elemento a "Yogur"
+
+// Mostrar la lista entera en consola
+console.log("Lista de compras actualizada:", listaCompras);
