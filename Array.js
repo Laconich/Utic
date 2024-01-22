2dcb44980c3e98c4e3a5f23945aff2e142841750
Laconich <jlaconich97@gmail.com>
2024-01-22 19:30:09 -0300

    agregar cambio

--- Array.js
+++ Array.js
@@ -1,8 +1,11 @@
-let razadeperros = ['perro','perrito','perrote',1200,false,null];
+let razadeperros = ['perro','perrito','perrote'];
 console.log({razadeperros});
 
-//const ultimoElemento = razadeperros
+const ultimoElemento = [razadeperros.length -1];
 
 //Uso de la propiedad lenght
 
 console.log(razadeperros.length);
+razadeperros.push('Perototote');
+razadeperros.push('Perotototototototte');
+console.log({razadeperros});
